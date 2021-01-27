c2c6893939aba6d9024036b25d7d45c8e0f4e98a
liShuangPeng <1210233@163.com>
2021-01-27 15:30:44 +0800

    运算符优先级group

--- Sources/SPKit.swift
+++ Sources/SPKit.swift
@@ -74,3 +74,68 @@ public final class OnBackground {
         return nil
     }
 }
+
+/*
+ 
+===============================================================================================================
+|     Operator     |            Description            |    Associativity    |        Precedence group        |
++==================+===================================+=====================+================================+
+|        <<        |  Bitwise left shift               |       None          |     BitwiseShiftPrecedence     |
+|        >>        |  Bitwise right shift              |       None          |     BitwiseShiftPrecedence     |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|         *        |  Multiply                         |  Left associative   |    MultiplicationPrecedence    |
+|         /        |  Divide                           |  Left associative   |    MultiplicationPrecedence    |
+|         %        |  Remainder                        |  Left associative   |    MultiplicationPrecedence    |
+|        &*        |  Multiply, ignoring overflow      |  Left associative   |    MultiplicationPrecedence    |
+|         &        |  Bitwise AND                      |  Left associative   |    MultiplicationPrecedence    |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|         +        |  Add                              |  Left associative   |       AdditionPrecedence       |
+|         -        |  Subtract                         |  Left associative   |       AdditionPrecedence       |
+|        &+        |  Add with overflow                |  Left associative   |       AdditionPrecedence       |
+|        &-        |  Subtract with overflow           |  Left associative   |       AdditionPrecedence       |
+|         |        |  Bitwise OR                       |  Left associative   |       AdditionPrecedence       |
+|         ^        |  Bitwise XOR                      |  Left associative   |       AdditionPrecedence       |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|        ..<       |  Half-open range                  |       None          |    RangeFormationPrecedence    |
+|        ...       |  Closed range                     |       None          |    RangeFormationPrecedence    |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|        is        |  Type check                       |  Left associative   |       CastingPrecedence        |
+| as, as?, and as! |  Type cast                        |  Left associative   |       CastingPrecedence        |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|        ??        |  Nil coalescing                   |  Right associative  |    NilCoalescingPrecedence     |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|         <        |  Less than                        |       None          |      ComparisonPrecedence      |
+|        <=        |  Less than or equal               |       None          |      ComparisonPrecedence      |
+|         >        |  Greater than                     |       None          |      ComparisonPrecedence      |
+|        >=        |  Greater than or equal            |       None          |      ComparisonPrecedence      |
+|        ==        |  Equal                            |       None          |      ComparisonPrecedence      |
+|        !=        |  Not equal                        |       None          |      ComparisonPrecedence      |
+|        ===       |  Identical                        |       None          |      ComparisonPrecedence      |
+|        !==       |  Not identical                    |       None          |      ComparisonPrecedence      |
+|        ~=        |  Pattern match                    |       None          |      ComparisonPrecedence      |
+|        .==       |  Pointwise equal                  |       None          |      ComparisonPrecedence      |
+|        .!=       |  Pointwise not equal              |       None          |      ComparisonPrecedence      |
+|        .<        |  Pointwise less than              |       None          |      ComparisonPrecedence      |
+|        .<=       |  Pointwise less than or equal     |       None          |      ComparisonPrecedence      |
+|        .>        |  Pointwise greater than           |       None          |      ComparisonPrecedence      |
+|        .>=       |  Pointwise greater than or equal  |       None          |      ComparisonPrecedence      |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|        &&        |  Logical AND                      |  Left associative   |  LogicalConjunctionPrecedence  |
+|        ||        |  Logical OR                       |  Left associative   |  LogicalDisjunctionPrecedence  |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|        ?:        |  Ternary conditional              |  Right associative  |       TernaryPrecedence        |
++------------------+-----------------------------------+---------------------+--------------------------------+
+|         =        |  Assign                           |  Right associative  |      AssignmentPrecedence      |
+|        *=        |  Multiply and assign              |  Right associative  |      AssignmentPrecedence      |
+|        /=        |  Divide and assign                |  Right associative  |      AssignmentPrecedence      |
+|        %=        |  Remainder and assign             |  Right associative  |      AssignmentPrecedence      |
+|        +=        |  Add and assign                   |  Right associative  |      AssignmentPrecedence      |
+|        -=        |  Subtract and assign              |  Right associative  |      AssignmentPrecedence      |
+|        <<=       |  Left bit shift and assign        |  Right associative  |      AssignmentPrecedence      |
+|        >>=       |  Right bit shift and assign       |  Right associative  |      AssignmentPrecedence      |
+|        &=        |  Bitwise AND and assign           |  Right associative  |      AssignmentPrecedence      |
+|        |=        |  Bitwise OR and assign            |  Right associative  |      AssignmentPrecedence      |
+|        ^=        |  Bitwise XOR and assign           |  Right associative  |      AssignmentPrecedence      |
++------------------+-----------------------------------+---------------------+--------------------------------+
+ 
+*/
